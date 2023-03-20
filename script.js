0ea592a3c79c5d69e7118217cac3514bbe032ce5
IwanMarcin <marcinm.iwan@gmail.com>
2023-03-20 20:43:12 +0100

    Added new features

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 const carouselSlide = document.querySelector(".gallery");
 const carouselImages = document.querySelectorAll(".gallery img");
 
+let changePhoto = document.getElementById("obrazek");
+
+changePhoto.style.backgroundImage = "url('./assets/photo3.jpg')";
+
+let first = "url('./assets/photo1.jpg')";
+let second = "url('./assets/photo2.jpg')";
+let third = "url('./assets/photo3.jpg')";
+let fourth = "url('./assets/photo4.jpg')";
+let fifth = "url('./assets/photo5.jpg')";
+let counter = 1;
+function updatePhoto() {
+  if (counter === 1) {
+    changePhoto.style.backgroundImage = first;
+    counter += 1;
+  } else if (counter === 2) {
+    changePhoto.style.backgroundImage = second;
+    counter += 1;
+  } else if (counter === 3) {
+    changePhoto.style.backgroundImage = third;
+    counter += 1;
+  } else if (counter === 4) {
+    changePhoto.style.backgroundImage = fourth;
+    counter += 1;
+  } else if (counter === 5) {
+    changePhoto.style.backgroundImage = fifth;
+    counter = 1;
+  }
+
+  setTimeout(() => {
+    updatePhoto();
+  }, 5000);
+}
+
+updatePhoto();
